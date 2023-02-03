b148914ed657a3396f884da288bc8498d471f0e2
bbautista73 <bbautista73@gmail.com>
2023-02-03 14:13:08 -0500

    restructed home page and nav

--- resources/js/infinitescroll.js
+++
@@ -1,9 +0,0 @@
-const scrollContainer = document.querySelector('.scroll-container');
-
-scrollContainer.addEventListener('mouseover', function() {
-this.style.overflowX = 'hidden';
-});
-
-scrollContainer.addEventListener('mouseout', function() {
-this.style.overflowX = 'scroll';
-});
